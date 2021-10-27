26fa270aa79591fa2d9529300edb1fd72ef5513e
MaralGS <79161220+MaralGS@users.noreply.github.com>
2021-10-27 12:47:14 +0200

    Project Change

---
+++ L06_Map_Collisions.Handout/Output/Assets/maps/Nature_environment_02.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="Nature_environment_02" tilewidth="48" tileheight="48" tilecount="176" columns="22">
+ <image source="Nature_environment_02.png" width="1100" height="400"/>
+</tileset>
