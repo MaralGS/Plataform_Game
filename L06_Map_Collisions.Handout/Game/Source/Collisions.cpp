4230b6534d337326bdcf7c7986969732f9abdd72
Pol Maresch <polmaresch@gmail.com>
2021-11-22 12:34:41 +0100

    mirar

--- L06_Map_Collisions.Handout/Game/Source/Collisions.cpp
+++ L06_Map_Collisions.Handout/Game/Source/Collisions.cpp
@@ -126,10 +126,10 @@ void Collisions::DebugDraw()
 			app->render->DrawRectangle(colliders[i]->rect, 0, 0, 255, alpha);
 			break;
 		case Collider::Type::PLAYER: // green
-			app->render->DrawRectangle(colliders[i]->rect,  255, 255, 0, alpha);
+			app->render->DrawRectangle(colliders[i]->rect,  0, 255, 0, alpha);
 			break;
 		case Collider::Type::GROUND: // red
-			app->render->DrawRectangle(colliders[i]->rect, 255, 255, 0, 80 );
+			app->render->DrawRectangle(colliders[i]->rect, 255, 0, 255, alpha );
 			break;
 		case Collider::Type::FINISH: // red
 			app->render->DrawRectangle(colliders[i]->rect, 255, 0, 0, alpha);
